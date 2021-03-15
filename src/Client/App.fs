6b8ecdc50bfef388afac788fb8bc771c8de86f59
gajaruban.jeyakumar@gmail.com <gajaruban.jeyakumar@gmail.com>
2021-03-16 00:22:14 +0100

    trying sutil...

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -3,12 +3,36 @@ open Sutil
 open Sutil.DOM
 open Sutil.Attr
 open Sutil.Bulma
+open Sutil.Styling
+open Feliz
+
+let css = [
+        rule "p" [
+            Css.color "orange"
+            Css.fontFamily "'Comic Sans MS', cursive"
+            Css.fontSize (length.em 2.0)
+        ]
+    ]
+
+let Nested() =
+   Html.p [ DOM.text "...don't affect this element" ] |> withStyle []
 
 // In Sutil, the view() function is called *once*
 let view() =
     Html.div [
-    DOM.text "Hello World!"
-]
+        Html.p [
+            DOM.text "These styles..."
+        ]
+        Nested()
+        Html.p [
+            DOM.text "just trying some shit,,"
+
+        ]
+
+        withStyle css <| Nested()
+
+    ]
+
 
 // Start the app
 view() |> mountElement "sutil-app"
