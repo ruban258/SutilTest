23173544372d4b60e1050a118069c8694944003e
gajaruban.jeyakumar@gmail.com <gajaruban.jeyakumar@gmail.com>
2021-03-19 22:31:21 +0100

    Stonk

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -15,7 +15,12 @@ let update (msg:Message) (model:Model) : Model = model
 
 let mainStyleSheet =
     Sutil.Bulma.withBulmaHelpers[
-        rule "nav.navbar" [Css.backgroundColor "#EEEEEE"]]
+        rule "nav.navbar" [
+            Css.backgroundColor "#EEEEEE"
+            Css.borderBottomColor "red"
+            Css.borderBottomWidth 1]
+        rule "div.body" [ Css.height 1000 ]
+        rule ".full-height" [ Css.height 1000 ]]
 
 
 module Navbar =
@@ -25,14 +30,25 @@ module Navbar =
         bulma.navbar[Navbar.brand [Navbar.item[ Html.text "STONK"]]]
 
 module Main =
+    let contentView =
+        bulma.section[color.hasBackgroundLight]
     let section =
-        bulma.columns
-           [bulma.column[column.is2; color.hasBackgroundLight];
-                bulma.column[column.is10]];
+        bulma.columns[
+            bulma.column[
+                class' "full-height"
+                column.is2
+                color.hasBackgroundLight]
+            bulma.column[
+                column.is10
+                class' "full-height"
+                contentView]]
 
 
 let view () =
-    Html.div [Navbar.section; Main.section]
+    Html.div [
+        class' "body";
+        Navbar.section;
+        Main.section]
     |> withStyle mainStyleSheet
 
 view () |> mountElement "sutil-app"
