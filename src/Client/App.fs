38d4069303325f02d47f2183fc9253572dc0a355
gajaruban.jeyakumar@gmail.com <gajaruban.jeyakumar@gmail.com>
2021-03-23 22:52:30 +0100

    Episode 4 ui

--- src/Client/App.fs
+++ src/Client/App.fs
@@ -10,9 +10,13 @@ open System
 [<Measure>]type percentage
 [<Measure>]type price
 type Symbol = Symbol of string
-type SharePrice = SharePrice of decimal<price>
+type StockPrice = StockPrice of decimal<price>
 type Quantity = Quantity of uint
 type ShareQty = ShareQty of Quantity
+[<RequireQualifiedAccess>]
+module ShareQty =
+    let get (ShareQty(Quantity num)) = num
+
 type Pnl = Pnl of decimal<percentage>
 type OpenPnl = OpenPnl of Pnl
 type DayPnl = DayPnl of Pnl
@@ -20,14 +24,31 @@ type PortfolioOpenPnl = PortfolioOpenPnl of OpenPnl
 type PortfolioDayPnl = PortfolioDayPnl of DayPnl
 type PositionOpenPnl = PositionOpenPnl of OpenPnl
 type PositionDayPnl = PositionDayPnl of DayPnl
+type CurrentStockPrice = CurrentStockPrice of StockPrice
+[<RequireQualifiedAccess>]
+module CurrentStockPrice =
+    let get (CurrentStockPrice(StockPrice num)) = num
+type LastClosePrice = LastClosePrice of StockPrice
+type AveragePrice= AveragePrice of decimal<price>
+type AverageOpenPrice = AverageOpenPrice of AveragePrice
+module AverageOpenPrice =
+    let get (AverageOpenPrice(AveragePrice num))= num
 type Balances = Undefined
+type Stock =
+    {
+        Symbol: Symbol
+        CurrentPrice: CurrentStockPrice
+        LastClosePrice: LastClosePrice
+    }
+[<RequireQualifiedAccess>]
+module Stock =
+    let getSymbolString ({Symbol = Symbol str}) =
+        str
 type PositionInfo =
     {
-        Symbol:Symbol
-        Price : SharePrice
+        Stock: Stock
         OpenQty: ShareQty
-        OpenPnl: PositionOpenPnl
-        DayPnl : PositionDayPnl
+        AverageOpenPrice: AverageOpenPrice
     }
 type Portfolio =
     {
@@ -35,34 +56,47 @@ type Portfolio =
         Balances: Balances
     }
 
-
-type SummaryInfo =
-    | Positions of PositionInfo List
+type PortfolioTab =
+    | Positions
     | Balances
 
 type Model =
     {
-        PortfolioOpenPnl: PositionOpenPnl
-        PortfolioDayPnl: PortfolioDayPnl
-        Portfoli:Portfolio
-        SelectedPane:SummaryInfo
+        Portfolio:Portfolio
+        CurrentPortfolioTab:PortfolioTab
     }
 
 type Message =
-    | SelectedPaneSelected of SummaryInfo
+    | SelectedPaneChanged of PortfolioTab
 
 let init ():Model =
+    let stock =
+        {
+            Symbol = Symbol "GME"
+            CurrentPrice = CurrentStockPrice (StockPrice 34.0M<price>)
+            LastClosePrice = LastClosePrice (StockPrice 50.0M<price>)
+        }
+    let positionInfo =
+        {
+            Stock = stock
+            OpenQty = ShareQty(Quantity 300u)
+            AverageOpenPrice = AverageOpenPrice(AveragePrice 45.9M<price>)
+        }
+    let portfolio =
+        {
+            Balances = Undefined
+            Positions = [positionInfo]
+        }
     {
-        OpenPnl = 3.0M<percentage>
-        DayPnl = -0.5M<percentage>
-        SelectedPane = Balances
+        Portfolio = portfolio
+        CurrentPortfolioTab = Positions
     }
 
 let update (msg:Message) (model:Model) : Model =
     match msg with
-    | SelectedPaneSelected summaryInfo ->
-        if summaryInfo <> model.SelectedPane then
-            {model with SelectedPane = summaryInfo}
+    | SelectedPaneChanged portfolioTab ->
+        if portfolioTab <> model.CurrentPortfolioTab then
+            {model with CurrentPortfolioTab = portfolioTab}
         else
             model
 
@@ -98,12 +132,13 @@ module Navbar =
             ]
         ]
 module SummaryPage =
-    let positionsTable =
+    let positionsTable (positionsStore:IObservable<PositionInfo list>)=
         let header =
             Html.thead[
                 Html.tr[
                     Html.th[Html.text "Symbol"]
-                    Html.th[Html.text "Price"]
+                    Html.th[Html.text "Open Price"]
+                    Html.th[Html.text "Current Price"]
                     Html.th[
                         Html.text "Qty"
                         Html.abbr[attr ("Title", "Open Quantity")]
@@ -114,10 +149,46 @@ module SummaryPage =
                     ]
                 ]
             ]
+        let getRowFromPositionInfo (position:PositionInfo) =
+            let openQtyString =
+                ShareQty.get position.OpenQty
+                |> string
+            let averageOpenPriceString =
+                AverageOpenPrice.get position.AverageOpenPrice
+                |> string
+            let currentPrice =
+                position.Stock.CurrentPrice
+                |> CurrentStockPrice.get
+                |> string
 
-        bulma.table [
-            header
-        ]
+            Html.tr[
+                Html.td[
+                    Stock.getSymbolString position.Stock
+                    |> Html.text
+                ]
+                Html.td[
+                    averageOpenPriceString
+                    |> Html.text
+                ]
+                Html.td[
+                    currentPrice
+                    |> Html.text
+                ]
+                Html.td[
+                    openQtyString
+                    |> Html.text
+                ]
+            ]
+        let rows (positions:PositionInfo list) =
+            positions
+            |> List.map getRowFromPositionInfo
+        let getTableFromPositions positions =
+            header::rows positions
+            |>bulma.table
+
+        Bind.fragment positionsStore getTableFromPositions
+
+
     let pnlElement (title:string) (percentage:decimal<percentage>) =
         let percentageSpan =
             Html.span[
@@ -138,17 +209,18 @@ module SummaryPage =
                 percentageSpan
             ]
         ]
-    let buttons summaryInfo (text:string) (isSelectedStore:IObservable<bool>) dispatch=
+    let buttons portfolioTab (isSelectedStore:IObservable<bool>) dispatch=
         let button =
             Bulma.Level.item[
                 Html.button[
-                    Html.text text
-                    onClick (fun e -> dispatch <| SelectedPaneSelected summaryInfo )[]
+                    string portfolioTab
+                    |> Html.text
+                    onClick (fun _ -> dispatch <| SelectedPaneChanged portfolioTab )[]
                     bindClass isSelectedStore "selected"
                 ]
             ]
         button
-    let level (selectedPaneStore:IObservable<SummaryInfo>) dispatch =
+    let level (selectedPaneStore:IObservable<PortfolioTab>) dispatch =
         let isPositionsSelected =
             selectedPaneStore
             |> Store.map (function Positions -> true | _ -> false)
@@ -157,22 +229,31 @@ module SummaryPage =
             |> Store.map (function Balances -> true | _ -> false)
         bulma.level[
             Bulma.Level.left [
-                buttons Positions "Positions" isPositionsSelected dispatch
-                buttons Balances "Balances" isBalancesSelected dispatch
+                buttons Positions isPositionsSelected dispatch
+                buttons Balances  isBalancesSelected dispatch
             ]
             Bulma.Level.right[
                 pnlElement "Open pnl" -0.3M<percentage>
                 pnlElement "Day pnl" 0.3M<percentage>
             ]
         ]
-    let ContentView model dispatch=
+    let ContentView (model:IStore<Model>) dispatch=
         let selectedPaneStore =
             model
-            |> Store.map(fun selectd -> selectd.SelectedPane)
+            |> Store.map(fun m -> m.CurrentPortfolioTab)
+            |> Store.distinct
+        let portfolioStore =
+            model
+            |> Store.map(fun m -> m.Portfolio)
             |> Store.distinct
 
         let getViewForSelectedPane = function
-        | Positions -> positionsTable
+        | Positions ->
+            let positionListStore =
+                 portfolioStore
+                |>Store.map(fun p -> p.Positions)
+                |>Store.distinct
+            positionsTable positionListStore
         | Balances -> Html.text "not done yet"
 
         bulma.section[
@@ -190,7 +271,7 @@ module SummaryPage =
             ]
         ]
 module Main =
-    let section model dispatch=
+    let section (model:IStore<Model>) dispatch=
         bulma.columns[
             bulma.column[
                 column.is2
